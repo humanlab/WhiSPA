ab33d1ea10f31017f58501bc57feab58072fd512
Jarhatz <rajathrao1001@gmail.com>
2024-11-09 17:47:12 -0500

    code cleanup

---
+++ sql/csv2sql.py
@@ -0,0 +1,102 @@
+import os, re
+import argparse
+import numpy as np
+import pandas as pd
+from mysql import connector
+
+
+DEBUG = True
+BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+
+
+"""
+python sql/csv2sql.py -c ~/.my.cnf -t "wtc_segment_persona" --csv /cronus_data/rrao/wtc_clinic/segment_outcomes.csv
+python sql/csv2sql.py -c ~/.my.cnf -t "hitop_segment_persona" --csv /cronus_data/rrao/hitop/segment_outcomes.csv
+python sql/csv2sql.py -c ~/.my.cnf -t "wtc_user_persona" --csv /cronus_data/rrao/wtc_clinic/user_agg_outcomes.csv
+python sql/csv2sql.py -c ~/.my.cnf -t "hitop_user_persona" --csv /cronus_data/rrao/hitop/user_agg_outcomes.csv
+"""
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Script to Convert CSV to SQL file')
+
+    parser.add_argument("-c", "--credential", required=True, type=str, help="Specify path to credential file")
+    parser.add_argument("-t", "--table_name", required=True, type=str, help="Specify the name of the SQL Table")
+    parser.add_argument("--csv", required=True, type=str, help="Specify the .csv file")
+    args = parser.parse_args()
+
+    # Create a connection to the MySQL server
+    connection, cursor = get_sql_credentials(args.credential)
+    driver(connection, cursor, args.table_name, args.csv)
+    
+
+# Verifies user's credentials and returns a MySQL Connection object
+def get_sql_credentials(filename):
+    usr = ''
+    pwd = ''
+    with open(filename, 'r') as file:
+        for line in file.readlines():
+            if line.startswith('user'):
+                usr = line[5:].strip()
+            if line.startswith('password'):
+                pwd = line[9:].strip()
+
+    connection = connector.connect(
+        host='localhost',
+        user=usr,
+        password=pwd,
+        database='HiTOP',
+    )
+    return connection, connection.cursor()
+
+
+# Function to map pandas data types to MySQL data types
+def map_dtype_to_mysql(dtype):
+    if pd.api.types.is_integer_dtype(dtype):
+        return 'INT'
+    elif pd.api.types.is_float_dtype(dtype):
+        return 'FLOAT'
+    elif pd.api.types.is_bool_dtype(dtype):
+        return 'BOOLEAN'
+    elif pd.api.types.is_datetime64_any_dtype(dtype):
+        return 'DATETIME'
+    else:
+        return 'TEXT'
+    
+
+def driver(connection, cursor, table_name, csv_path):
+    df = pd.read_csv(csv_path, encoding='utf-8').dropna().reset_index(drop=True)
+    # df['segment_message'] = df['segment_message'].apply(lambda x: re.sub(r'[^\x00-\x7F]+', '', x) if isinstance(x, str) else x)
+
+    # Dynamically create SQL columns based on DataFrame dtypes
+    columns = []
+    for col_name, dtype in df.dtypes.items():
+        if col_name == 'user_id':
+            if pd.api.types.is_integer_dtype(dtype):
+                mysql_type = 'INT'
+            else:
+                mysql_type = 'VARCHAR(24)'
+        elif col_name == 'segment_id':
+            mysql_type = 'VARCHAR(24)'
+        elif col_name == 'segment_filename':
+            mysql_type = 'VARCHAR(100)'
+        else:
+            mysql_type = map_dtype_to_mysql(dtype)
+        columns.append(f"{col_name} {mysql_type}")
+
+    create_query = f"CREATE TABLE {table_name} ({', '.join(columns)});"
+    cursor.execute(create_query)
+
+    for idx, row in df.iterrows():
+        print(f'[{idx + 1}/{len(df)}] Inserted: {row.values[0]}')
+        placeholders = ', '.join(['%s'] * len(row))
+        insert_query = f"INSERT INTO {table_name} VALUES ({placeholders})"
+        cursor.execute(insert_query, tuple(row))
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+if __name__ == '__main__':
+    main()
--- sql/fill_table.py
+++ sql/fill_table.py
@@ -10,8 +10,8 @@ BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
 
 """
-python sql/fill_table.py -c ~/.my.cnf -t "feat\$sbert384\$wtc_seg_outcomes\$user_id" --csv /cronus_data/rrao/WhiSBERT/embeddings/wtc_all-MiniLM-L12-v2.csv
-python sql/fill_table.py -c ~/.my.cnf -t "feat\$sbert384\$hitop_seg_outcomes\$user_id" --csv /cronus_data/rrao/WhiSBERT/embeddings/hitop_all-MiniLM-L12-v2.csv
+python sql/fill_table.py -c ~/.my.cnf -t "feat\$sbert384\$wtc_seg_persona\$user_id" --csv /cronus_data/rrao/WhiSBERT/embeddings/wtc_all-MiniLM-L12-v2.csv
+python sql/fill_table.py -c ~/.my.cnf -t "feat\$sbert384\$hitop_seg_persona\$user_id" --csv /cronus_data/rrao/WhiSBERT/embeddings/hitop_all-MiniLM-L12-v2.csv
 """
 
 
@@ -21,11 +21,12 @@ def main():
     parser.add_argument("-c", "--credential", required=True, type=str, help="Specify path to credential file")
     parser.add_argument("-t", "--table_name", required=True, type=str, help="Specify the name of the SQL Table to insert to")
     parser.add_argument("--csv", required=True, type=str, help="Specify the path to the features (must be .csv file format)")
+    parser.add_argument("--no_agg", action="store_true", help="Flag to disallow aggregation of features by `user_id`")
     args = parser.parse_args()
 
     # Create a connection to the MySQL server
     connection, cursor = get_sql_credentials(args.credential)
-    driver(connection, cursor, args.table_name, args.csv)
+    driver(connection, cursor, args.table_name, args.csv, args.no_agg)
     
 
 # Verifies user's credentials and returns a MySQL Connection object
@@ -48,17 +49,17 @@ def get_sql_credentials(filename):
     return connection, connection.cursor()
 
 
-def driver(connection, cursor, table_name, csv_path):
+def driver(connection, cursor, table_name, csv_path, no_agg):
     if os.path.basename(csv_path).startswith('hitop'):
         segments_path = '/cronus_data/rrao/hitop/segment_outcomes.csv'
-        user_id_dtype = 'VARCHAR(12)'
+        group_id_dtype = 'VARCHAR(24)'
     elif os.path.basename(csv_path).startswith('wtc'):
         segments_path = '/cronus_data/rrao/wtc_clinic/segment_outcomes.csv'
-        user_id_dtype = 'INT'
+        group_id_dtype = 'VARCHAR(24)' if no_agg else 'INT'
     try:
         create_query = f"""
         CREATE TABLE {table_name} (
-            group_id {user_id_dtype},
+            group_id {group_id_dtype},
             feat VARCHAR(4),
             value DOUBLE,
             group_norm DOUBLE,
@@ -81,20 +82,27 @@ def driver(connection, cursor, table_name, csv_path):
 
     df = pd.read_csv(segments_path)[['user_id', 'segment_id']].merge(pd.read_csv(csv_path), on='segment_id', how='left')
 
-    user_ids = np.unique(df['user_id'])
-    for idx, user_id in enumerate(user_ids):
-        print(f'[{idx + 1}/{len(user_ids)}]\tuser_id: {user_id}')
-        mean_feats = df[df['user_id'] == user_id].iloc[:, 2:].mean()
+    if no_agg:
+        for idx, row in df.iterrows():
+            print(f'[{idx + 1}/{len(df)}]\tsegment_id: {row["segment_id"]}')
+            for feat_name, value in row[2:].items():
+                values = (row['segment_id'], feat_name, value, value)
+                cursor.execute(insert_query, values)
+    else:
+        user_ids = np.unique(df['user_id'])
+        for idx, user_id in enumerate(user_ids):
+            print(f'[{idx + 1}/{len(user_ids)}]\tuser_id: {user_id}')
+            mean_feats = df[df['user_id'] == user_id].iloc[:, 2:].mean()
 
-        if user_id_dtype == 'INT':
-            user_id = int(user_id)
+            if group_id_dtype == 'INT':
+                user_id = int(user_id)
 
-        for feat_name, value in mean_feats.items():
-            values = (user_id, feat_name, value, value)
-            cursor.execute(insert_query, values)
+            for feat_name, value in mean_feats.items():
+                values = (user_id, feat_name, value, value)
+                cursor.execute(insert_query, values)
 
-        # Commit the changes to the database
-        connection.commit()
+    # Commit the changes to the database
+    connection.commit()
 
     # Close the cursor and connection
     cursor.close()
